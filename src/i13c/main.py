bc047c9f3c0f007e20d529821aed2901879c3380
Adrian Macal <amacal@id5.io>
2025-12-19 09:19:11 +0000

    Add sample .i13c code

--- src/i13c/main.py
+++ src/i13c/main.py
@@ -1,6 +1,22 @@
 import click
 
+from i13c import lex
 
-@click.command()
+
+@click.group()
 def i13c():
-    click.echo("Hello from i13c!")
+    pass
+
+
+@i13c.command()
+@click.argument("path", type=click.Path(exists=True))
+def tokenize(path: str) -> None:
+    with open(path, "r", encoding="utf-8") as f:
+        text = f.read()
+
+    lexer = lex.open_text(text)
+    tokens = lex.tokenize(lexer)
+
+    for token in tokens:
+        extracted = lexer.extract(token)
+        click.echo(f"{token.code:03}:{token.offset:04}:{token.length:02} -> {extracted!r}")
