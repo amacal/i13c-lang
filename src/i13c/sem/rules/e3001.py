c84e9a177a928acf8890005579f816304a384aba
Adrian Macal <amacal@id5.io>
2025-12-27 18:04:01 +0000

    Handle 3001.

--- src/i13c/sem/rules/e3001.py
+++ src/i13c/sem/rules/e3001.py
@@ -1,26 +1,26 @@
 from typing import List
 
-from i13c import diag
+from i13c import diag, err
 from i13c.sem.model import SemanticGraph
+from i13c.sem.asm import Immediate
 
 
 def validate_immediate_out_of_range(graph: SemanticGraph) -> List[diag.Diagnostic]:
     diagnostics: List[diag.Diagnostic] = []
 
-    # for snippet in graph.snippets.values():
-    #     for instruction in snippet.instructions:
-    #         for operand in instruction.operands:
-    #             if operand.kind == b"immediate":
+    for instruction in graph.instructions.values():
+        for operand in instruction.operands:
+            if operand.kind == b"immediate":
 
-    #                 # satisfy type checker
-    #                 assert isinstance(operand.target, Immediate)
+                # satisfy type checker
+                assert isinstance(operand.target, Immediate)
 
-    #                 if operand.target.width is None:
-    #                     diagnostics.append(
-    #                         err.report_e3001_immediate_out_of_range(
-    #                             instruction.ref,
-    #                             operand.target.value,
-    #                         )
-    #                     )
+                if operand.target.width is None:
+                    diagnostics.append(
+                        err.report_e3001_immediate_out_of_range(
+                            instruction.ref,
+                            operand.target.value,
+                        )
+                    )
 
     return diagnostics
