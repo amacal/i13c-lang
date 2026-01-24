68a7eb09a9e433702f008ef01b26c5124e5ffa8d
Adrian Macal <amacal@id5.io>
2026-01-24 13:44:06 +0000

    Add dataflow for entries.

--- src/i13c/sem/nodes/indices/dataflows.py
+++ src/i13c/sem/nodes/indices/dataflows.py
@@ -2,9 +2,11 @@ from typing import Dict, List
 
 from i13c.core.mapping import OneToOne
 from i13c.sem.infra import Configuration
-from i13c.sem.typing.entities.functions import FunctionId
-from i13c.sem.typing.indices.controlflows import FlowGraph, FlowNode
+from i13c.sem.typing.entities.functions import Function, FunctionId
+from i13c.sem.typing.entities.parameters import ParameterId
+from i13c.sem.typing.indices.controlflows import FlowEntry, FlowGraph, FlowNode
 from i13c.sem.typing.indices.dataflows import DataFlow
+from i13c.sem.typing.indices.variables import VariableId
 
 
 def configure_dataflow_by_flownode() -> Configuration:
@@ -13,20 +15,36 @@ def configure_dataflow_by_flownode() -> Configuration:
         produces=("indices/dataflow-by-flownode",),
         requires=frozenset(
             {
+                ("functions", "entities/functions"),
                 ("controlflows", "indices/flowgraph-by-function"),
+                ("variables", "indices/variables-by-parameter"),
             }
         ),
     )
 
 
 def build_dataflows(
+    functions: OneToOne[FunctionId, Function],
     controlflows: OneToOne[FunctionId, FlowGraph],
+    variables: OneToOne[ParameterId, VariableId],
 ) -> OneToOne[FunctionId, FlowGraph]:
 
+    # found dataflows for each entry
     dataflows: Dict[FlowNode, DataFlow] = {}
 
-    for _, flows in controlflows.items():
-        for node in flows.nodes():
-            dataflows[node] = DataFlow(declares=[], reads=[], writes=[])
+    for fid, function in functions.items():
+        flowgraph = controlflows.get(fid)
+        entry: FlowEntry = flowgraph.entry
+
+        for node in flowgraph.nodes():
+            dataflows[node] = DataFlow(
+                declares=[],
+                reads=[],
+                writes=[],
+            )
+
+        for pid in function.parameters:
+            vid = variables.get(pid)
+            dataflows[entry].declares.append(vid)
 
     return OneToOne[FunctionId, FlowGraph].instance(dataflows)
