86d36365babbea3cd91d3063913b7c9f5a99a7ec
Adrian Macal <amacal@id5.io>
2026-04-10 09:03:55 +0000

    Visit types in the syntax.

--- src/tests/semantic/syntax/types.py
+++ src/tests/semantic/syntax/types.py
@@ -11,6 +11,16 @@ def can_visit_ranges_in_a_snippet_signature() -> None:
     assert len(list(visitor.graph.ranges.items())) == 1
 
 
+def can_visit_type_in_a_snippet_signature() -> None:
+    visitor = parse_syntax_graph(
+        """
+            asm main(v@rax: u8) { mov rax, 0x1234; }
+        """
+    )
+
+    assert len(list(visitor.graph.types.items())) == 1
+
+
 def can_visit_ranges_in_a_function_signature() -> None:
     visitor = parse_syntax_graph(
         """
@@ -19,6 +29,16 @@ def can_visit_ranges_in_a_function_signature() -> None:
     )
 
     assert len(list(visitor.graph.ranges.items())) == 1
+
+
+def can_visit_type_in_a_function_signature() -> None:
+    visitor = parse_syntax_graph(
+        """
+            fn main(v: u8) { foo(v); }
+        """
+    )
+
+    assert len(list(visitor.graph.types.items())) == 1
 
 
 def can_visit_ranges_in_a_value_declaration() -> None:
@@ -31,3 +51,15 @@ def can_visit_ranges_in_a_value_declaration() -> None:
     )
 
     assert len(list(visitor.graph.ranges.items())) == 1
+
+
+def can_visit_type_in_a_value_declaration() -> None:
+    visitor = parse_syntax_graph(
+        """
+            fn main() {
+                val v: u8 = 0x01;
+            }
+        """
+    )
+
+    assert len(list(visitor.graph.types.items())) == 1
